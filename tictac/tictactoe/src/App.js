9ad5710c7f9fec007c7879f358a082a6f35e33ca
souvik666 <peacedynasty0@gmail.com>
2021-10-22 21:47:25 +0530

    tictactoe react

---
+++ tictac/tictactoe/src/App.js
@@ -0,0 +1,14 @@
+import { Button, InputGroup, FormControl } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import "./index.css";
+import Game from "../src/Components/Game";
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  );
+}
+
+export default App;
---
+++ tictac/tictactoe/src/Components/Square.js
@@ -0,0 +1,10 @@
+const Square = ({ value, onClick }) => {
+  const style = value ? `squares ${value}` : `squares`;
+
+  return (
+    <button className={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+export default Square;
